a459f4d99df12ef8d0abe607cc95fc3656931b59
Paxon Cheung <paxon.cheung@gmail.com>
2021-01-12 07:40:31 +0000

    updating work

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
 let testVar = middle([1, 2, 3, 4])
 let testVar2 = middle([1, 2, 3, 4, 5, 6])
@@ -7,8 +7,25 @@ let testVar3 = middle([1, 2, 3])
 let testVar4 = middle([1, 2, 3, 4, 5])
 let testVar5 = middle([1])
 
-assertArraysEqual((testVar), [2, 3]);
-assertArraysEqual((testVar2), [3, 4]);
-assertArraysEqual((testVar3), [2]);
-assertArraysEqual((testVar4), [3]);
-assertArraysEqual((testVar5), []);
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual((testVar), [2, 3]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual((testVar2), [3, 4]); 
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual((testVar3), [2]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual((testVar4), [3]);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual((testVar5), []);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
+const tail   = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[0], "Lighthouse"); 
+  });
+
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+});
